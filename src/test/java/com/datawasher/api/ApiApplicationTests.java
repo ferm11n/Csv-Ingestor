5201eabfe38784227761fb587fb1dd20777f3168
ferm11n <lucasferminnieto@yahoo.com.ar>
2026-01-08 22:43:33 -0300

    Inicio del proyecto

---
+++ src/test/java/com/datawasher/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawasher.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
