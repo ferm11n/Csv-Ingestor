ba65a4a771781b148c93033b828418c847832c5a
ferm11n <lucasferminnieto@yahoo.com.ar>
2026-02-16 23:33:25 -0300

    agregamos tabla para visualizar los datos

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [ 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatTableModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Data Washer';
   message = '';
+
+  //variables para la tablita
+  headers: string[] = [];
+  rows: any[] = [];
+  showTable = false;
 
   //Aun que esten private, igual se crean automaticamente
   constructor(private http: HttpClient, private cd: ChangeDetectorRef) {}
@@ -36,17 +48,29 @@ export class AppComponent {
 
     this.http.post<any>('http://localhost:8080/api/files/upload', formData).subscribe({
       next: (response) => {
-        console.log('Datos recibidos:', response); 
-        
+        //console.log('Datos recibidos:', response); 
+        this.headers = response.headers;
+        this.rows = response.rows;
+        this.showTable = true;
+
         this.message = `✅ ¡Archivo subido con éxito! ID: ${response.fileId}`;
-                
         this.cd.detectChanges();
       },
       error: (error) => {
         console.error('Error:', error);
         this.message = '❌ Error al subir el archivo.';
+        this.showTable = false;
         this.cd.detectChanges();
       }
     });
+  }
+
+  //los placeholder
+  applyRule(ruleType: string) {
+    alert("Aca va la regla: ${ruleType}");
+  }
+
+  downloadFile() {
+    alert("Aca se descarga el archivo limpio")
   }
 }
